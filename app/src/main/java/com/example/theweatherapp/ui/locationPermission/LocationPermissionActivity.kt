01d2b8abba612ec4e95755c27a21fa05ae424415
Divya Patel <divyap.brainerhib@gmail.com>
2026-04-02 15:03:23 +0530

    - Add binding in location file - Add home fragment kt files

--- app/src/main/java/com/example/theweatherapp/ui/locationPermission/LocationPermissionActivity.kt
+++ app/src/main/java/com/example/theweatherapp/ui/locationPermission/LocationPermissionActivity.kt
@@ -18,7 +18,9 @@ class LocationPermissionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_location_permission)
+        binding = ActivityLocationPermissionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
