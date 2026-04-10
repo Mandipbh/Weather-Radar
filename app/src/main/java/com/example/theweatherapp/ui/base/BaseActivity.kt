76a1e6dc00838a0a8d9e77577f708aec896e6206
Kavita <mandeepv.brainerhub@gmail.com>
2026-04-10 15:27:50 +0530

    Full Screen view & status bar issue fixed

--- app/src/main/java/com/example/theweatherapp/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/theweatherapp/ui/base/BaseActivity.kt
@@ -1,11 +1,18 @@
 package com.example.theweatherapp.base
 
 import android.content.Context
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.theweatherapp.utils.LocaleHelper
 import com.example.theweatherapp.utils.PrefManager
 
 open class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
 
     override fun attachBaseContext(newBase: Context) {
         val lang = PrefManager.getLanguage(newBase)
